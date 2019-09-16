a6571c592295772a323a2dc2db3589dd40885bfc
Oskar Mendel <brokenprogrammer@gmail.com>
2019-09-16 07:00:19 +0200

    Hopefully fixes tabbing issue..?

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -15,19 +15,19 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
 
-typedef float			r32; // TODO(Oskar): Better naming for floating point?
-typedef double			r64;
-typedef unsigned char 	ubyte;
-typedef unsigned int 	uint;
-typedef int8_t 			s8;
-typedef uint8_t 		u8;
-typedef int16_t 		s16;
-typedef uint16_t 		u16;
-typedef int32_t 		s32;
-typedef uint32_t 		u32;
-typedef int64_t 		s64;
-typedef uint64_t 		u64;
-typedef s32 			b32;
+typedef float				r32;
+typedef double				r64;
+typedef unsigned char 		ubyte;
+typedef unsigned int 		uint;
+typedef int8_t 				s8;
+typedef uint8_t 			u8;
+typedef int16_t 			s16;
+typedef uint16_t 			u16;
+typedef int32_t 			s32;
+typedef uint32_t 			u32;
+typedef int64_t 			s64;
+typedef uint64_t 			u64;
+typedef s32 				b32;
 
 #define internal 		static
 #define local_persist 	static
