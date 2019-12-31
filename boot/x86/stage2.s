98408d8cb609348511c28fbd9bb3afefbc30a220
Oskar Mendel <brokenprogrammer@gmail.com>
2019-12-31 13:04:12 +0100

    Added check for PCI support.

--- boot/x86/stage2.s
+++ boot/x86/stage2.s
@@ -27,7 +27,16 @@ start:
     mov [drive_number],dl
     mov [partition_entry],si
 
-; TODO(Oskar): PCI
+check_pci:
+    ; Check if the computer has PCI available. "int 0x1A, AX=0xB101 (PCI BIOS presence check)"
+    mov ax,0xB101
+    xor	edi,edi ; NOTE(Oskar): Zero out EDI according to: http://www.ctyme.com/intr/rb-2371.htm
+    int	0x1A
+    mov	si,error_no_pci
+    jc error
+    or ah,ah
+    jnz error
+
 ; TODO(Oskar): Video Mode
 ; TODO(Oskar): Upper & Lower Memory
 ; TODO(Oskar): A20
@@ -35,14 +44,27 @@ start:
 ; TODO(Oskar): Paging
 ; TODO(Oskar): GDT
 ; TODO(Oskar): Load Kernel
-
-
     cli
     hlt
+
+error:
+	; Print an error message
+	lodsb
+	or	al,al
+	jz	.break
+	mov	ah,0xE
+	int	0x10
+	jmp	error
+
+	; Break indefinitely
+	.break:
+	cli
+	hlt
 
 drive_number: db 0
 partition_entry: dw 0
 
 page_table_allocation_location: dq 0x2000
 
+error_no_pci: db "Error: Could not find the PCI bus",0
 msg:	db	"Starting stage 2 boot...",13,10,0
